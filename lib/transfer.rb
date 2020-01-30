fc97bc69b1fc285aba89f5e3437092e36321e38f
mattjagiello <mjagiello@gmail.com>
2020-01-30 13:06:26 -0600

    Updated bank_account and transfer

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  def initialize(sender, receiver, amount = 50, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      return true
+    end
+  end
+
+  def execute_transaction
+    if self.status != "complete" && self.sender.balance >= amount && self.valid?
+        self.sender.balance -= amount
+        self.receiver.balance += amount
+        self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      if self.sender.balance > amount
+        self.sender.balance += amount
+        self.receiver.balance -= amount
+        self.status = "reversed"
+      end
+    end
+  end
 end
